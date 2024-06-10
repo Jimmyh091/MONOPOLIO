8bf2ab67d41b79d0c3ece5bfd33876c73aab175c
Jimmyh091 <jaimecaballero164@gmail.com>
2024-06-10 18:47:15 +0200

    Crear jugador

--- MONOPOLIO/src/monopolio/MONOPOLIO.java
+++ MONOPOLIO/src/monopolio/MONOPOLIO.java
@@ -3,18 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package monopolio;
-
+import java.util.Scanner;
 /**
  *
  * @author jaime
  */
 public class MONOPOLIO {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    public static Jugador[] crearJugadores(){
+        System.out.println("Cuantos jugadores van a jugar?");
+        byte numJug = sc.nextByte();
+        
+        Jugador[] jugadores = new Jugador[numJug];
+        
+        for (byte i = 0; i < jugadores.length; i++) {
+            jugadores[i] = crearJugador(i);
+        }
+        
+        return jugadores;
+    }
+    public static Jugador crearJugador(byte i){
+        System.out.println("Que nombre tiene tu jugador?");
+        String nombre = sc.nextLine();
+        
+        return new Jugador(i, nombre);
+    }
 }
